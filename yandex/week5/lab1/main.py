c920713b4b28017e373710ca45919903d61b4088
ArtyomKaltovich <kaltovichartyom@gmail.com>
2016-05-12 23:28:49 +0300

    adding time Measurement to yandex week5.1

--- yandex/week5/lab1/main.py
+++ yandex/week5/lab1/main.py
@@ -3,6 +3,8 @@ import pandas
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.cross_validation import KFold, cross_val_score
 from numpy import mean
+import time
+
 PACKAGE_PARENT = "../.."
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
@@ -27,11 +29,14 @@ def load_data(path):
 
 
 X, y = load_data('abalone.csv')
+start = time.time()
 for i in range(1, 51):
 	clf = RandomForestRegressor(n_estimators=i, random_state=1)
 	kf = KFold(len(y), n_folds=5, random_state=1, shuffle=True)
-	score = mean(cross_val_score(clf, X, y, cv=kf, scoring='r2', n_jobs=-1))
-	print(i, score) 
+	score = mean(cross_val_score(clf, X, y, cv=kf, scoring='r2', n_jobs=1))
+	#print(i, score) 
 	if (score > 0.52):
 		io_yandex.print_result(str(i), "1.txt")
 		break
+end = time.time()
+print(end - start)
